0b3025c1db37d00c60a3862de7e78be9389f8de3
AhmadAmin <ahmad.amin.ma@gmail.com>
2018-08-14 15:19:51 +0200

    First Commit

---
+++ 6DOFTrackingUnity/Assets/Scripts/VuforiaCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VuforiaCamera : MonoBehaviour {
+    public GameObject BackgroundTexture;
+    public Text text;
+	// Use this for initialization
+	void Start () {
+        Camera.main.clearFlags = CameraClearFlags.Skybox;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(BackgroundTexture!=null && BackgroundTexture.activeInHierarchy)
+        {
+            BackgroundTexture.SetActive(false);
+        }
+        if(BackgroundTexture==null)
+        {
+            BackgroundTexture = GameObject.Find("BackgroundPlane");
+        }
+
+        if(text!=null)
+            text.text = transform.position.ToString();
+	}
+}
